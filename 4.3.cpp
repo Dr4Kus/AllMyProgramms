90c8f9c94e4b0d0f00d57e0745b6cba5abb97b51
Drakus <zenyka27z@gmail.com>
2023-04-26 20:46:24 +0400

    + main

--- 4.3.cpp
+++ 4.3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 class TPen {
 public:
@@ -10,7 +12,26 @@ public:
 		Fcolor[2] = b;
 	}
 
+	void get() {
+		std::cout << "	Color: " << Fcolor[0] << " " << Fcolor[1] << " " << Fcolor[2] << std::endl;
+	}
+};
+
 int main() {
+	std::ifstream input("colors.txt");
+	TPen pens[3];
+
+	for (int i = 0; i < 3; i++) {
+		int r, g, b;
+		input >> r >> g >> b;
+		pens[i].set(r, g, b);
+	}
+	input.close();
+
+	for (int i = 0; i < 3; i++) {
+		pens[i].get();
+	}
 
 	return 0;
 }
+

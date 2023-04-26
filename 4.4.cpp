7185f2f49f376ec9f682e016df3e43622fbfd6c5
Drakus <zenyka27z@gmail.com>
2023-04-26 20:50:00 +0400

    + class TBook

--- 4.4.cpp
+++ 4.4.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class TBook {
+private:
+	string Name;
+	string Author;
+	unsigned int Count;
 
 
 int main() {
 	
+
 	return 0;
 }
+

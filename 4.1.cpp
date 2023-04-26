1c88910ee736d0b3c4302734aec265f3b04bd38a
Drakus <zenyka27z@gmail.com>
2023-04-26 20:39:42 +0400

    + class TPen

--- 4.1.cpp
+++ 4.1.cpp
@@ -1,3 +1,15 @@
-int main() {	
-return 0;
+#include <iostream>
+#include <string>
+
+class TPen {
+public:
+	std::string Fcolor;
+	TPen(std::string color) {
+		Fcolor = color;
+	};
+};
+
+	int main() {
+		
+	return 0;
 }

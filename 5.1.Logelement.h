42221c45986474f77380e0c14c184df9dd3287e0
Drakus <zenyka27z@gmail.com>
2023-05-15 08:51:00 +0400

    Added class LogElement

---
+++ 5.1.Logelement.h
@@ -0,0 +1,18 @@
+using namespace std;
+class LogElement {
+private:
+	bool In1;
+	bool In2;
+protected:
+	bool Res;
+public:
+	void setIn1(bool newIn1);
+	void setIn2(bool newIn2);
+	bool getResOr();
+	bool getResNot();
+	bool getResAnd();
+bool getResNand();
+bool getResNor;
+ 	bool getResXor;
+}; 
+

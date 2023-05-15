ffcce1f47ac58118181e7d624230a59180808909
Drakus <zenyka27z@gmail.com>
2023-05-15 08:56:59 +0400

    Added output Result

--- 5.1.main.cpp
+++ 5.1.main.cpp
@@ -6,5 +6,8 @@ int main () {
 	LogElement logel;
 	logel.setIn1(true);
 	logel.setIn2(false);
-	
+	cout << " 1 or 0 = " << logel.getResOr() << endl;
+	cout << " Not 1 = " << logel.getResNot()<< endl; 
+	cout << " 1 and 0 = " << logel.getResAnd()<< endl; 
+	return 0 ;
 }
